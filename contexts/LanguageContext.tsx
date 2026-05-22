ea45e358b7d70059e1383476b2d14ce84a87737b
Misumi Matsudo <misumi0921@gmail.com>
2026-05-22 22:37:54 +0900

    Switch default language to English

--- contexts/LanguageContext.tsx
+++ contexts/LanguageContext.tsx
@@ -13,7 +13,7 @@ interface LanguageContextType {
 const LanguageContext = createContext<LanguageContextType | undefined>(undefined);
 
 export function LanguageProvider({ children }: { children: ReactNode }) {
-  const [language, setLanguage] = useState<Language>('ja');
+  const [language, setLanguage] = useState<Language>('en');
 
   const t = (key: string): any => {
     const keys = key.split('.');
